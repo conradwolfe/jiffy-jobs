8b55c642b5b947ad1caff8c647bcd4100b3d0a41
awongway <awong20@hawaii.edu>
2018-11-14 20:57:27 -1000

    updated with logo

--- app/imports/ui/components/TopMenu.jsx
+++ app/imports/ui/components/TopMenu.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Container, Menu, Dropdown, Icon } from 'semantic-ui-react';
+import { Container, Menu, Dropdown, Icon, Image } from 'semantic-ui-react';
 
 export default class TopMenu extends React.Component {
   render() {
     return (
-        <Menu borderless className="topmenu">
+        <Menu borderless>
           <Container>
-            <Menu.Item fitted position ="right"><Icon name="user circle"/></Menu.Item>
-          <Dropdown item text='Profile'>
-            <Dropdown.Menu>
-              <Dropdown.Item>Matches</Dropdown.Item>
-              <Dropdown.Item>Automotive</Dropdown.Item>
-              <Dropdown.Item>Home</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+            <Menu.Item fitted><Image src="/images/jiffylogo.png" rounded size="small"/></Menu.Item>
+            <Menu.Item fitted position ="right"><Icon size="big" name="user circle"/></Menu.Item>
+            <Menu.Item fitted ><Icon size="big" name="home"/></Menu.Item>
+            <Menu.Item fitted ><Icon size="big" name="suitcase"/></Menu.Item>
+            <Menu.Item fitted ><Icon size="big" name="search"/></Menu.Item>
           </Container>
         </Menu>
     );
