f97339499fcc8059c41c491561bd800f1952cd0a
Cade Yamamoto <cadehy@hawaii.edu>
2018-11-27 15:04:02 -1000

    Company profile data collection complete
    Added new publication "CompanyProfileInfo" to fetch data from one specific user.

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -36,23 +36,41 @@ class NavBar extends React.Component {
                 </Form.Group>
               </Form>
             </Menu.Item>
-            <Menu.Item compact position="right" as={NavLink} exact to="/cprofile"><Icon size="large" name="user circle"/></Menu.Item>
+            {(() => {
+              if (Roles.userIsInRole(Meteor.userId(), 'company')) {
+                return (
+                    <Menu.Item compact position="right" as={NavLink} exact to="/cprofile">
+                      <Icon size="large" name="user circle"/></Menu.Item>
+                );
+              } if (Roles.userIsInRole(Meteor.userId(), 'student')) {
+                return (
+                    <Menu.Item compact position="right" as={NavLink} exact to="/sprofile">
+                      <Icon size="large" name="user circle"/></Menu.Item>
+                );
+              } else {
+                return (
+                    <Menu.Item compact position="right" as={NavLink} exact to="/errorpage">
+                    <Icon size="large" name="user circle"/></Menu.Item>
+                );
+              }
+            })()}
             <Menu.Item compact as={NavLink} exact to="/errorpage"><Icon size="large" name="home"/></Menu.Item>
-              {(() => {
-                  if (Roles.userIsInRole(Meteor.userId(), 'company')) {
-                      return (
-                          <Menu.Item compact as={NavLink} exact to="/cdash"><Icon size="large" name="suitcase"/></Menu.Item>
-                      )
-                  } else if (Roles.userIsInRole(Meteor.userId(), 'student')) {
-                      return (
-                          <Menu.Item compact as={NavLink} exact to="/sdash"><Icon size="large" name="suitcase"/></Menu.Item>
-                      )
-                  } else {
-                      return (
-                          <Menu.Item compact as={NavLink} exact to="/errorpage"><Icon size="large" name="suitcase"/></Menu.Item>
-                      )
-                  }
-              })()}
+            {(() => {
+              if (Roles.userIsInRole(Meteor.userId(), 'company')) {
+                return (
+                    <Menu.Item compact as={NavLink} exact to="/sdash"><Icon size="large" name="suitcase"/></Menu.Item>
+                );
+              } if (Roles.userIsInRole(Meteor.userId(), 'student')) {
+                return (
+                    <Menu.Item compact as={NavLink} exact to="/cdash"><Icon size="large" name="suitcase"/></Menu.Item>
+                );
+              } else {
+                return (
+                    <Menu.Item compact as={NavLink} exact to="/errorpage"><Icon size="large"
+                                                                                name="suitcase"/></Menu.Item>
+                );
+              }
+            })()}
             <Menu.Item compact>
               {this.props.currentUser === '' ? (
                   <Dropdown icon='th' floating labeled button>
--- app/imports/ui/pages/CompanyProfile.jsx
+++ app/imports/ui/pages/CompanyProfile.jsx
@@ -1,102 +1,105 @@
 import React from 'react';
-import {Grid, Image, Header, Divider, Container, Button} from 'semantic-ui-react';
-import {Form} from 'semantic-ui-react/dist/commonjs/collections/Form/Form';
-import {Dropdown} from 'semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown';
+import { Grid, Image, Header, Divider, Loader } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
-import {Meteor} from 'meteor/meteor';
-import {withTracker} from 'meteor/react-meteor-data';
-import {withRouter, NavLink} from 'react-router-dom';
-import {CompanyInfo} from '/imports/api/companyinfo/companyinfo';
+import { Meteor } from 'meteor/meteor';
+import { withTracker } from 'meteor/react-meteor-data';
+// import {withRouter, NavLink} from 'react-router-dom';
+import { CompanyInfo } from '/imports/api/companyinfo/companyinfo';
 
 /** A simple static component to render some text for the profile page. */
 class CompanyProfile extends React.Component {
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Loading Profile</Loader>;
+  }
 
-    render() {
-        const divStyle = {paddingTop: '50px', paddingBottom: '150px'};
-        return (
-            <div>
-                <div className="profile-page">
-                    <Grid container verticalAlign='center' centered columns={2}>
-                        <Grid.Column verticalAlign='left'>
-                            <Grid.Row>
-                                <Image size='medium' centered
-                                       src={"https://react.semantic-ui.com/images/avatar/large/matthew.png"}/>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Header size="huge" as='h2' icon textAlign='left'>
-                                    <div className="UserFirstAndLast">
-                                        Rating
-                                    </div>
-                                </Header>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <p>
-                                    94%
-                                </p>
-                            </Grid.Row>
-                            <Grid.Row>
-                                <Header size="huge" as='h2' icon textAlign='left'>
-                                    <div className="UserFirstAndLast">
-                                        Contact Information
-                                    </div>
-                                </Header>
-                            </Grid.Row>
-                        </Grid.Column>
-                        <Grid.Column verticalAlign='right'>
-                            <div className="bio-stuff">
-                                <Grid.Row>
-                                    <div>
-                                        <Header size="huge" as='h2' icon textAlign='left'>
-                                            <div className="UserFirstAndLast">
-                                                Edit name
-                                            </div>
-                                        </Header>
-                                    </div>
-                                </Grid.Row>
-                                <Divider/>
-                                <Grid.Row>
-                                    <Header size="huge" as='h2' icon textAlign='left'>
-                                        <div className="UserFirstAndLast">
-                                            BIO
-                                        </div>
-                                    </Header>
-                                    <p> Edit Description </p>
-                                </Grid.Row>
-                                <Divider/>
-                                <Grid.Row>
-                                    <Header size="huge" as='h2' icon textAlign='left'>
-                                        <div className="UserFirstAndLast">
-                                            Interests
-                                        </div>
-                                    </Header>
-                                    <p> Affert iudico oblique ea vim. Pri an fierent principes, perfecto definitionem an
-                                        has, in cibo accusam sit. Eam doming putent ei. No alii populo vituperatoribus
-                                        duo, est mentitum voluptua ex, veri aliquip prompta sit ei. Mei brute dicam
-                                        scripta eu, nisl oportere cu pro.
-                                        Duo ex graeco mediocrem consequuntur, eos adhuc nostrud fuisset at. Fierent
-                                        abhorreant ea sit. Te nec sint audiam concludaturque, cu quod aeterno torquatos
-                                        eam. Cum dolore ignota conceptam ex, vim ne urbanitas comprehensam
-                                        necessitatibus. Nam graeci consequuntur ut, congue iudicabit nam ad. Cum te
-                                        purto dicat fuisset, cum te audiam prodesset comprehensam.
-                                    </p>
-                                </Grid.Row>
-                            </div>
-                        </Grid.Column>
-                    </Grid>
+  renderPage() {
+    // const divStyle = {paddingTop: '50px', paddingBottom: '150px'};
+    return (
+        <div>
+          <div className="profile-page">
+            <Grid container verticalAlign='middle' columns={2}>
+              <Grid.Column verticalAlign='left'>
+                <Grid.Row>
+                  <Image size='medium' centered
+                         src={this.props.data.image}/>
+                </Grid.Row>
+                <Grid.Row>
+                  <Header size="huge" as='h2' icon textAlign='left'>
+                    <div className="landing-text-dark">
+                      Rating
+                    </div>
+                  </Header>
+                </Grid.Row>
+                <Grid.Row>
+                  <p>
+                    94%
+                  </p>
+                </Grid.Row>
+                <Grid.Row>
+                  <Header size="huge" as='h2' icon textAlign='left'>
+                    <div className="landing-text-dark">
+                      Contact Information
+                    </div>
+                  </Header>
+                </Grid.Row>
+              </Grid.Column>
+              <Grid.Column verticalAlign='right'>
+                <div className="bio-stuff">
+                  <Grid.Row>
+                    <div>
+                      <Header size="huge" as='h2' icon textAlign='left'>
+                        <div className="landing-text-dark">
+                          {this.props.data.companyName}
+                        </div>
+                      </Header>
+                    </div>
+                  </Grid.Row>
+                  <Divider/>
+                  <Grid.Row>
+                    <Header size="huge" as='h2' icon textAlign='left'>
+                      <div className="landing-text-dark">
+                        BIO
+                      </div>
+                    </Header>
+                    <p> {this.props.data.description} </p>
+                  </Grid.Row>
+                  <Divider/>
+                  <Grid.Row>
+                    <Header size="huge" as='h2' icon textAlign='left'>
+                      <div className="landing-text-dark">
+                        Interests
+                      </div>
+                    </Header>
+                    <p> Affert iudico oblique ea vim. Pri an fierent principes, perfecto definitionem an
+                      has, in cibo accusam sit. Eam doming putent ei. No alii populo vituperatoribus
+                      duo, est mentitum voluptua ex, veri aliquip prompta sit ei. Mei brute dicam
+                      scripta eu, nisl oportere cu pro.
+                      Duo ex graeco mediocrem consequuntur, eos adhuc nostrud fuisset at. Fierent
+                      abhorreant ea sit. Te nec sint audiam concludaturque, cu quod aeterno torquatos
+                      eam. Cum dolore ignota conceptam ex, vim ne urbanitas comprehensam
+                      necessitatibus. Nam graeci consequuntur ut, congue iudicabit nam ad. Cum te
+                      purto dicat fuisset, cum te audiam prodesset comprehensam.
+                    </p>
+                  </Grid.Row>
                 </div>
-            </div>
+              </Grid.Column>
+            </Grid>
+          </div>
+        </div>
 
-        );
-    }
+    );
+  }
 }
 
 CompanyProfile.propTypes = {
-    ready: PropTypes.bool.isRequired,
+  data: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
 export default withTracker(() => {
-    const subscription = Meteor.subscribe('CompanyInfo');
-    return {
-        ready: subscription.ready(),
-    };
+  const subscription = Meteor.subscribe('CompanyProfileInfo');
+  return {
+    data: CompanyInfo.find({}).fetch()[0],
+    ready: subscription.ready(),
+  };
 })(CompanyProfile);
