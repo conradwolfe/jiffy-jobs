b0824dad4cbf4c69726f41dfad8d5d493c243ca5
joelsikkink <joelsikkink@gmail.com>
2018-11-29 01:35:09 -1000

    Student Profiles work now

--- app/imports/ui/pages/StudentProfile.jsx
+++ app/imports/ui/pages/StudentProfile.jsx
@@ -1,98 +1,120 @@
 import React from 'react';
-import { Grid, Image, Header, Divider, Container, Button } from 'semantic-ui-react';
-import { Form } from 'semantic-ui-react/dist/commonjs/collections/Form/Form';
-import { Dropdown } from 'semantic-ui-react/dist/commonjs/modules/Dropdown/Dropdown';
+import { Grid, Image, Header, List, Divider, Button, Loader, Segment, Icon, Label } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import { withRouter, NavLink } from 'react-router-dom';
-import { CompanyInfo } from '/imports/api/companyinfo/companyinfo';
+import { StudentInfo } from '/imports/api/studentinfo/studentinfo';
 
 /** A simple static component to render some text for the profile page. */
 class StudentProfile extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-    this.deleteCallback = this.deleteCallback.bind(this);
+  render() {
+    return (this.props.ready) ? this.renderPage() : <Loader active>Loading Profile</Loader>;
   }
 
-  render() {
-    const divStyle = { paddingTop: '50px', paddingBottom: '150px' };
+  renderPage() {
     return (
         <div>
           <div className="profile-page">
-            <Grid container verticalAlign='left' centered columns={2}>
-              <Grid.Column>
-                <Container>
-                  <div className="img-info">
-                    <Grid container verticalAlign='center' centered rows={2}>
-                      <Grid.Row>
-                        <Image size='medium' floated='right' src={this.props.companyinfo.image}/>
-                      </Grid.Row>
-                      <Grid.Row>
-                        <Grid container verticalAlign='left' centered columns={2}>
-                          <Grid.Column>
-                            <Header size="huge" as='h2' icon textAlign='left'>
-                              <div className="UserFirstAndLast">
-                                Rating
-                              </div>
-                            </Header>
-                            <p>
-                              94%
-                            </p>
-                          </Grid.Column>
-                      <Divider/>
-                        <Grid.Column>
-                          <Header size="huge" as='h2' icon textAlign='left'>
-                            <div className="UserFirstAndLast">
-                              Contact Information
-                            </div>
-                          </Header>
-                        </Grid.Column>
-                        </Grid>
-                      </Grid.Row>
-                    </Grid>
-                  </div>
-                </Container>
-              </Grid.Column>
-              <Grid.Column>
-                <Container>
-                    <div className="bio-stuff">
-                      <Grid container verticalAlign='center' centered rows={3}>
-                      <Grid.Row>
-                        <div>
-                          <Header size="huge" as='h2' icon textAlign='left'>
-                            <div className="UserFirstAndLast">
-                              {this.props.companyinfo.companyName}
-                            </div>
-                          </Header>
-                        </div>
-                      </Grid.Row>
-                        <Divider/>
-                      <Grid.Row>
-                        <Header size="huge" as='h2' icon textAlign='left'>
-                          <div className="UserFirstAndLast">
-                            BIO
-                          </div>
-                        </Header>
-                        <p> {this.props.companyinfo.description}</p>
-                      </Grid.Row>
-                        <Divider/>
-                      <Grid.Row>
-                        <Header size="huge" as='h2' icon textAlign='left'>
-                          <div className="UserFirstAndLast">
-                            Interests
-                          </div>
-                        </Header>
-                        <p> Affert iudico oblique ea vim. Pri an fierent principes, perfecto definitionem an has, in cibo accusam sit. Eam doming putent ei. No alii populo vituperatoribus duo, est mentitum voluptua ex, veri aliquip prompta sit ei. Mei brute dicam scripta eu, nisl oportere cu pro.
-
-                          Duo ex graeco mediocrem consequuntur, eos adhuc nostrud fuisset at. Fierent abhorreant ea sit. Te nec sint audiam concludaturque, cu quod aeterno torquatos eam. Cum dolore ignota conceptam ex, vim ne urbanitas comprehensam necessitatibus. Nam graeci consequuntur ut, congue iudicabit nam ad. Cum te purto dicat fuisset, cum te audiam prodesset comprehensam.
-                        </p>
-                      </Grid.Row>
-                      </Grid>
+            <Grid>
+              <Grid.Column width={4} className='profile-column' >
+                <Segment>
+                  <Grid.Row>
+                    <Image rounded size='medium'
+                           src={this.props.data.image}/>
+                  </Grid.Row>
+                  <Divider hidden />
+                  <Grid.Row>
+                    <div>
+                      <Icon name='location arrow' color='blue' />
+                      {this.props.data.gradyear}
                     </div>
-                </Container>
+                    <Divider hidden />
+                  </Grid.Row>
+                  <Divider hidden />
+                  <Grid.Row>
+                    <Header size="huge" as='h2'>
+                      <div className="landing-text-dark">
+                        Contact Information
+                      </div>
+                    </Header>
+                    <List>
+                      <List.Item>
+                        <List.Icon name='mail' color='blue' />
+                        <List.Content>john@foo.com</List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <List.Icon name='globe' color='blue' />
+                        <List.Content>website</List.Content>
+                      </List.Item>
+
+                      <List.Item>
+                        <List.Icon name='phone' color='blue' />
+                        <List.Content>(123)-456-7890</List.Content>
+                      </List.Item>
+                    </List>
+                  </Grid.Row>
+                  <Divider hidden />
+                  <Grid.Row>
+                    <Button as='div' labelPosition='right'>
+                      <Button basic color='blue'>
+                        <Icon name='add' />
+                      </Button>
+                      <Label as='a' basic color='blue' pointing='left'>
+                        Add to List
+                      </Label>
+                    </Button>
+                  </Grid.Row>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width='8' className="profile-column">
+                <Segment className="bio-stuff">
+                  <Grid.Row>
+                    <div>
+                      <Header size="huge" as='h2' icon>
+                        <div className="landing-text-dark">
+                          {this.props.data.firstName}
+                        </div>
+                        <div className="landing-text-dark">
+                          {this.props.data.lastName}
+                        </div>
+                      </Header>
+                    </div>
+                  </Grid.Row>
+                  <Divider/>
+                  <Grid.Row>
+                    <Header size="huge" as='h1' icon>
+                      <div className="landing-text-dark">
+                        About Me
+                      </div>
+                    </Header>
+                    <p> {this.props.data.description} </p>
+                  </Grid.Row>
+                  <Divider/>
+                  <Grid.Row>
+                    <Header size="huge" as='h2' icon>
+                      <div className="landing-text-dark">
+                        Interests
+                      </div>
+                    </Header>
+                    <p> Affert iudico oblique ea vim. Pri an fierent principes, perfecto definitionem an
+                      has, in cibo accusam sit. Eam doming putent ei. No alii populo vituperatoribus
+                      duo, est mentitum voluptua ex, veri aliquip prompta sit ei. Mei brute dicam
+                      scripta eu, nisl oportere cu pro.
+                      Duo ex graeco mediocrem consequuntur, eos adhuc nostrud fuisset at. Fierent
+                      abhorreant ea sit. Te nec sint audiam concludaturque, cu quod aeterno torquatos
+                      eam. Cum dolore ignota conceptam ex, vim ne urbanitas comprehensam
+                      necessitatibus. Nam graeci consequuntur ut, congue iudicabit nam ad. Cum te
+                      purto dicat fuisset, cum te audiam prodesset comprehensam.
+                    </p>
+                  </Grid.Row>
+                </Segment>
+              </Grid.Column>
+              <Grid.Column width="3" className="profile-column">
+                <Segment circular size="huge" color='blue'>
+                  <Header as='h2'>Rating</Header>
+                  <Header as='h3'>94%</Header>
+                </Segment>
               </Grid.Column>
             </Grid>
           </div>
@@ -103,8 +125,14 @@ class StudentProfile extends React.Component {
 }
 
 StudentProfile.propTypes = {
-  companyinfo: PropTypes.object.isRequired,
-  currentUser: PropTypes.string,
+  data: PropTypes.array.isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
-export default withRouter(StudentProfile);
+export default withTracker(() => {
+  const subscription = Meteor.subscribe('StudentProfileInfo');
+  return {
+    data: StudentInfo.find({}).fetch()[0],
+    ready: subscription.ready(),
+  };
+})(StudentProfile);
