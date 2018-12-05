869ed10875b702cb930ea2606681f56421bdf633
joelsikkink <joelsikkink@gmail.com>
2018-12-05 10:40:03 -1000

    Added changes to add roles

--- app/imports/startup/server/accounts.js
+++ app/imports/startup/server/accounts.js
@@ -3,7 +3,6 @@ import { Accounts } from 'meteor/accounts-base';
 import { Roles } from 'meteor/alanning:roles';
 
 /* eslint-disable no-console */
-
 function createUser(firstname, lastname, email, password, usertype) {
   console.log(`  Creating user ${email}.`);
   const userID = Accounts.createUser({
@@ -23,7 +22,27 @@ function createUser(firstname, lastname, email, password, usertype) {
     Roles.addUsersToRoles(userID, 'company');
   }
 }
-
+Meteor.methods({
+  'serverCreateUser': function(firstname, lastname, email, password, usertype) {
+    console.log(`  Creating user ${email}.`);
+    const userID = Accounts.createUser({
+      username: email,
+      firstname: firstname,
+      lastname: lastname,
+      email: email,
+      password: password,
+    });
+    if (usertype === 'admin') {
+      Roles.addUsersToRoles(userID, 'admin');
+    }
+    if (usertype === 'student') {
+      Roles.addUsersToRoles(userID, 'student');
+    }
+    if (usertype === 'company') {
+      Roles.addUsersToRoles(userID, 'company');
+    }
+  },
+});
 /** When running app for first time, pass a settings file to set up a default user account. */
 if (Meteor.users.find().count() === 0) {
   if (Meteor.settings.defaultAccounts) {
